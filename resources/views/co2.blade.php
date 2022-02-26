ca21c7113c6788edd0a3e6133c11490c2e4dcd27
chrispab <cbattisson@gmail.com>
2022-02-26 00:12:08 +0000

    ajax

--- resources/views/co2.blade.php
+++ resources/views/co2.blade.php
@@ -35,12 +35,18 @@
     <div class="row">
         <div class="column left-side">
             <h2>Sensor</h2>
-            <canvas id="co2-gauge" data-type="radial-gauge" data-width="250" data-height="250" data-units="CO2 ppm" data-title="false" data-value="500" data-min-value="400" data-max-value="1000" data-major-ticks="400,500,600,700,800,900,1000" data-minor-ticks="2" data-stroke-ticks="false" data-value-int="1" data-value-dec="0" data-font-value="courier" data-highlights='[
+            <canvas id="co2-gauge" data-type="radial-gauge" data-width="250" data-height="250" data-units="CO2 ppm"
+                data-title="false" data-value="500" data-min-value="400" data-max-value="1000"
+                data-major-ticks="400,500,600,700,800,900,1000" data-minor-ticks="2" data-stroke-ticks="false"
+                data-value-int="1" data-value-dec="0" data-font-value="courier" data-highlights='[
       { "from": 400, "to": 700, "color": "rgba(0,255,0,.5)" },
       { "from": 700, "to": 800, "color": "rgba(255,198,0,.7)" },
       { "from": 800, "to": 1000, "color": "rgba(255,0,0,.5)" }
 
-  ]' data-color-plate="#222" data-color-major-ticks="#f5f5f5" data-color-minor-ticks="#ddd" data-color-title="#fff" data-color-units="#ccc" data-color-numbers="#eee" data-color-needle-start="rgba(240, 128, 128, 1)" data-color-needle-end="rgba(255, 160, 122, .9)" data-value-box="true" data-animation-rule="bounce" data-animation-duration="500" data-animated-value="true"></canvas>
+  ]' data-color-plate="#222" data-color-major-ticks="#f5f5f5" data-color-minor-ticks="#ddd" data-color-title="#fff"
+                data-color-units="#ccc" data-color-numbers="#eee" data-color-needle-start="rgba(240, 128, 128, 1)"
+                data-color-needle-end="rgba(255, 160, 122, .9)" data-value-box="true" data-animation-rule="bounce"
+                data-animation-duration="500" data-animated-value="true"></canvas>
         </div>
         <div class="column middle">
             <!-- <h2>middle</h2> -->
@@ -89,7 +95,8 @@
             </div>
 
             <div>
-                <meter class="co2_meter" id="meter_value" min="400" low="700" high="800" max="1000" optimum="600" value="500"></meter>
+                <meter class="co2_meter" id="meter_value" min="400" low="700" high="800" max="1000" optimum="600"
+                    value="500"></meter>
             </div>
             <p></p>
 
@@ -125,6 +132,14 @@
                 <div>
                     <button class="button" type="button" onclick="loadLast_n_HoursData(72)">72 hours</button>
                 </div>
+
+            </div>
+            <div>
+                <!-- Rounded switch -->
+                <label class="switch">
+                    <input type="checkbox">
+                    <span class="slider round"></span>
+                </label>
             </div>
         </div>
 
